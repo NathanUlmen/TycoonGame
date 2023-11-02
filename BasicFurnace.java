8ef577f3aa6cb58b01cb2c3d0ef19f59243711f6
NathanUlmen <Ulmennathan@gmail.com>
2023-11-02 00:36:19 -0600

    Updated ToDo

--- BasicFurnace.java
+++ BasicFurnace.java
@@ -1,13 +1,9 @@
 import java.math.BigInteger;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
 
 public class BasicFurnace extends Furnace{
 
     public BasicFurnace(double processSpeed, String furnaceName, String processEffect, CircularOreArray<Ore> sharedOreArray, Player player) {
         super(5, "Basic Furnace", " + 0", sharedOreArray, player);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
